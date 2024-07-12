c11ee7b7326ed12e3e6eb90c8474176c601c3914
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-07-12 16:31:14 +0100

    connect mongodb database + set up server actions + create question & user & tag models

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,6 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
+import { createQuestion } from "@/lib/actions/question.action";
 
 const type: any = "create";
 
@@ -38,12 +39,15 @@ const Question = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
 
     try {
       // make an async call to your API -> create a question
       // contain all form data
+
+      await createQuestion({});
+
       // navigate to home page
     } catch (error) {
     } finally {
@@ -131,6 +135,8 @@ const Question = () => {
                     // @ts-ignore
                     editorRef.current = editor;
                   }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
                     height: 350,
