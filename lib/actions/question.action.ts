f6906e1fc05096952b14f6502747f63f79f15f84
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-07-13 21:11:26 +0100

    fetch question & render them to homepage

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,11 +1,29 @@
 "use server";
 
-import { connectToDatabase } from "../mongoose";
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
-export async function createQuestion(params: any) {
-  // eslint-disable-next-line no-empty
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
+
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase();
 
@@ -39,5 +57,7 @@ export async function createQuestion(params: any) {
     // Create an interaction record for the user's ask_question action
 
     // Increment author's reputation by +5 for creating a question
+
+    revalidatePath(path);
   } catch (error) {}
 }
