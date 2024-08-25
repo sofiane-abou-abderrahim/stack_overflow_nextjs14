9c7ade70e3196ff7d45e558620dea48062cb7c64
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-08-25 11:45:09 +0100

    implement profile page with user stats and with question & answer tabs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Navbar from "@/components/shared/navbar/Navbar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
+import LeftSidebar from "@/components/shared/LeftSidebar-1";
 import RightSidebar from "@/components/shared/RightSidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
