ea4c3bea068ec1c845c5e73e724b1cf531d8ea75
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-10-23 00:08:53 +0100

    fetch jobs from rapid api & display job cards

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -1,8 +1,36 @@
+import JobCard from "@/components/cards/JobCard";
 import JobsFilter from "@/components/jobs/JobsFilter";
-import { fetchCountries } from "@/lib/actions/job.action";
+import Pagination from "@/components/shared/Pagination";
 
-const Page = async () => {
+import {
+  fetchCountries,
+  fetchJobs,
+  fetchLocation,
+} from "@/lib/actions/job.action";
+
+import { Job } from "@/types";
+
+interface Props {
+  searchParams: {
+    q: string;
+    location: string;
+    page: string;
+  };
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const userLocation = await fetchLocation();
+
+  const jobs = await fetchJobs({
+    query:
+      searchParams.q && searchParams.location
+        ? `${searchParams.q}, ${searchParams.location}`
+        : `Software Engineer in ${userLocation}`,
+    page: searchParams.page ?? 1,
+  });
+
   const countries = await fetchCountries();
+  const page = parseInt(searchParams.page ?? 1);
 
   return (
     <>
@@ -11,6 +39,26 @@ const Page = async () => {
       <div className="flex">
         <JobsFilter countriesList={countries} />
       </div>
+
+      <section className="light-border mb-9 mt-11 flex flex-col gap-9 border-b pb-9">
+        {jobs.length > 0 ? (
+          jobs.map((job: Job) => {
+            if (job.job_title && job.job_title.toLowerCase() !== "undefined")
+              return <JobCard key={job.id} job={job} />;
+
+            return null;
+          })
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 w-full text-center">
+            Oops! We couldn&apos;t find any jobs at the moment. Please try again
+            later
+          </div>
+        )}
+      </section>
+
+      {jobs.length > 0 && (
+        <Pagination pageNumber={page} isNext={jobs.length === 10} />
+      )}
     </>
   );
 };
